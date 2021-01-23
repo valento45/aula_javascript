9e39a286c80a3a7ed80a4d3da1c8f62f6f27bfd2
igor helio <iggor1935@hotmail.com>
2021-01-23 15:37:52 -0300

    arrays, List, joins, push, pop, reverse

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,22 @@
-var nome = "Igor Helio";
+//var nome = "Igor Helio";
 
-alert("Primeiro js do " + nome.toString());
-           
+//alert("Primeiro js do " + nome.toString());
+           //console.log(nome);
+           //var listFrutas = ["maça", "abacaxi", "banana", "moranga", "uva"];
+           //listFrutas.push("laranja"); // Adiciona um ou mais elementos 
+           //listFrutas.pop(); //Remove o ultimo elemento
+           //listFrutas.reverse();// Retorna os elementos em ordem oposta a que estava, o 1 vira o ultimo, etc
+          // console.log(listFrutas.toString());
+        //   console.log(listFrutas.reverse().join(", ")); // Join pega os elementos de um array, e transforma em string separados pelo caractere passado como parametro
+       
+       /* alert("Olá, usuario. \r\nBem vindo à loja da comunidade.");
+        var Produto = {nome:"Camiseta Oakley", preco:"30.00"};
+        console.log(Produto.nome + " \nPor apenas: R$" + Produto.preco);
+        */
+
+        var frutas = [{nome:"abacaxi", cor:"verde amarelo"}, {nome:"banana", cor:"amarelo"}, {nome:"uva", cor:"roxa"}];
+        frutas.push({nome:" teste", cor:"verde"});
+        console.log(frutas);
+        console.log("\r\nFruta " + frutas[0].nome + " | cor " + frutas[0].cor);
+
+
